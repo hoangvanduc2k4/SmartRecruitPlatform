2339c3c5de206edc275dd0075dbe00d956893c16
hoangvanduc2k4 <hoangduc060704@gmail.com>
2026-03-09 12:58:27 +0700

    Thêm giao diện Save job

--- SmartRecruit.Application/Interfaces/Services/ISavedJobService.cs
+++ SmartRecruit.Application/Interfaces/Services/ISavedJobService.cs
@@ -6,6 +6,7 @@ namespace SmartRecruit.Application.Interfaces.Services
     public interface ISavedJobService
     {
         Task<bool> ToggleSaveJobAsync(long jobId, long userId);
+        Task<bool> IsJobSavedAsync(long jobId, long userId);
         Task<PagedList<JobResponse>> GetSavedJobsAsync(long userId, int page, int pageSize);
     }
 }
